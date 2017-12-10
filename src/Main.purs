a5bb0970fd1064de2fb4cc875d2d38ab4973b2c2
craigdallimore <decoy9697@gmail.com>
2017-12-11 08:34:14 +1300

    Introduce halogen

--- src/Main.purs
+++ src/Main.purs
@@ -3,19 +3,29 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.AVar (AVAR)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import DOM (DOM)
 import DOM.Classy.Element (fromElement, toElement)
 import DOM.Event.EventTarget (addEventListener, eventListener)
 import DOM.Event.Types (Event)
-import DOM.HTML.Event.EventTypes (click)
 import DOM.HTML (window)
-import DOM.HTML.Types (HTMLButtonElement, htmlDocumentToParentNode)
+import DOM.HTML.Event.EventTypes (click)
+import DOM.HTML.Types (HTMLButtonElement, HTMLDivElement, htmlDocumentToParentNode, htmlDivElementToHTMLElement)
 import DOM.HTML.Window (document)
 import DOM.Node.ParentNode (QuerySelector(QuerySelector), querySelector)
 import DOM.Node.Types (ParentNode, elementToEventTarget)
-import Data.Maybe (Maybe, maybe)
+import Data.Maybe (Maybe(..), maybe)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Halogen.VDom.Driver (runUI)
 
+-- [ ] Add README, LiCENCE
 -- [ ] chrome devtools bridge to android phone
 -- [ ] button click -> prompt for microphone
 -- [ ] with microphone access, show record button
@@ -24,10 +34,81 @@ import Data.Maybe (Maybe, maybe)
 -- [ ] 'play audio' button
 -- [ ] 'stop' button
 -- [ ] PWA stuff - how can we make this installable / good on android?
+  -- [ ] web app manifest
+  -- [ ] icons
+
+-- UX
+-- If we fail to have the required browser features, we should show a "please use another browser"
+-- Given the required features are available, we see
+-- - a record button
+-- - an empty list of past recordings
+
+-- Clicking the record button will
+-- - prompt for the ability to use the microphone (if it is not already given)
+-- - start recording
+-- - change the record button to a "stop" button
+-- - present a recording waveform
+
+-- Clicking the stop button should
+-- - add a new recording to the list
+-- - hide the stop button
+-- - show the record button
+
+-- list items should include
+-- - the ability to name a recording
+-- - change the start and end
+-- - remove the recording
+-- - present the waveform
+-- - play once
+-- - play on loop
+
 
 -------------------------------------------------------------------------------
 
-data Elements = Elements HTMLButtonElement
+type State = Boolean
+
+data Query a = Toggle a | IsOn (Boolean -> a)
+
+type Input = Unit
+
+data Message = Toggled Boolean
+
+btnOne :: forall m. H.Component HH.HTML Query Input Message m
+btnOne = H.component
+  { initialState: const initialState
+  , render
+  , eval
+  , receiver : const Nothing
+  }
+  where
+  initialState :: State
+  initialState = false
+
+  render :: State -> H.ComponentHTML Query
+  render state =
+    let
+      label = if state then "On" else "Off"
+    in
+      HH.button [ HP.title label
+                , HE.onClick (HE.input_ Toggle)
+                ]
+                [ HH.text label ]
+
+  eval :: Query ~> H.ComponentDSL State Query Message m
+  eval = case _ of
+    Toggle next -> do
+      state <- H.get
+      let nextState = not state
+      H.put nextState
+      H.raise $ Toggled nextState
+      pure next
+    IsOn reply -> do
+      state <- H.get
+      pure (reply state)
+
+-------------------------------------------------------------------------------
+
+data Elements = Elements HTMLButtonElement HTMLDivElement
 
 -------------------------------------------------------------------------------
 
@@ -37,17 +118,43 @@ x = const (log "click")
 queryElements :: forall eff. ParentNode -> Eff (dom :: DOM | eff) (Maybe Elements)
 queryElements docNode = do
 
-  btn <- (=<<) fromElement <$> querySelector (QuerySelector "button") docNode
+  recordBtn   <- (=<<) fromElement <$> querySelector (QuerySelector ".btn--record") docNode
+  halogenRoot <- (=<<) fromElement <$> querySelector (QuerySelector ".halogen-root") docNode
 
-  pure $ Elements <$> btn
+  pure $ Elements <$> recordBtn <*> halogenRoot
 
-bindDOM :: forall eff. Elements -> Eff (dom :: DOM, console :: CONSOLE | eff) Unit
-bindDOM (Elements button) = addEventListener click listener true eventTarget
-  where eventTarget = (elementToEventTarget <<< toElement) button
-        listener    = eventListener x
+bindDOM
+  :: forall eff
+   . Elements
+  -> Eff
+    ( dom       :: DOM
+    , console   :: CONSOLE
+    , avar      :: AVAR
+    , exception :: EXCEPTION
+    , ref       :: REF
+    | eff
+    ) Unit
+bindDOM (Elements button root) = do
+  addEventListener click listener true eventTarget
+  HA.runHalogenAff (runUI btnOne unit (htmlDivElementToHTMLElement root))
 
-main :: forall eff. Eff (dom :: DOM, console :: CONSOLE | eff) Unit
+  where
+    eventTarget = (elementToEventTarget <<< toElement) button
+    listener    = eventListener x
+
+
+main
+  :: forall eff
+   . Eff
+    ( dom       :: DOM
+    , console   :: CONSOLE
+    , avar      :: AVAR
+    , exception :: EXCEPTION
+    , ref       :: REF
+    | eff
+    ) Unit
 main = window
   >>= document
   >>= (queryElements <<< htmlDocumentToParentNode)
-  >>= (maybe (log "Could not access elements") bindDOM)
+  >>= maybe (log "Could not access elements") bindDOM
+
